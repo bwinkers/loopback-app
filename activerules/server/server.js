0e1599d495545e2192ed97a8994a4abe76ad2ce5
Brian Winkers <bwinkers@gmail.com>
2017-07-01 20:07:21 -0700

    Start of Passport support

--- activerules/server/server.js
+++ activerules/server/server.js
@@ -5,6 +5,41 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+// Passport configurators..
+var loopbackPassport = require('loopback-component-passport');
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+/*
+ * body-parser is a piece of express middleware that
+ *   reads a form's input and stores it as a javascript
+ *   object accessible through `req.body`
+ *
+ */
+var bodyParser = require('body-parser');
+
+/**
+ * Flash messages for passport
+ *
+ * Setting the failureFlash option to true instructs Passport to flash an
+ * error message using the message given by the strategy's verify callback,
+ * if any. This is often the best approach, because the verify callback
+ * can make the most accurate determination of why authentication failed.
+ */
+var flash      = require('express-flash');
+
+// Build the providers/passport config
+var config = {};
+try {
+	config = require('../providers.json');
+} catch (err) {
+	console.trace(err);
+	process.exit(1); // fatal
+}
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
