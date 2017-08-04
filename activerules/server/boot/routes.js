bf0ea3e3e6e6c876f85a590efd1c811ab13e8f46
Brian Winkers <bwinkers@gmail.com>
2017-08-04 02:50:06 -0700

    Fixed load activerules-view-resolver

--- activerules/server/boot/routes.js
+++ activerules/server/boot/routes.js
@@ -1,11 +1,22 @@
 'use strict';
 
-var avr = require('../activerules-view-resolver');
+var path = require('path');
 
+// ActiveRules default view directory.
+// Some modules or routes will have default views in a different directory.
+var defaultViewRoot = path.resolve('./server/views/');
+// ActiveRules root, config, l10n and site specfifc view may exist below here.
+var arRoot = path.resolve('./common/ar/config');
+
+// ActiveRules view resolver will test for site view overrides
+var avr = require('../activerules-view-resolver');
+avr.init(defaultViewRoot, arRoot);
+ 
+// ROUTES
 module.exports = function(app) {
     // Homepage
   app.get('/', function(req, res) {
-    avr.resolvePage('home', req, '../views/')
+    avr.resolvePage('home', req, defaultViewRoot)
         .then(function(html) {
           res.send(html);
         });
