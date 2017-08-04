ff9944d4ffe97489e359b87b4bb2b10df935b45d
Brian Winkers <bwinkers@gmail.com>
2017-08-04 02:51:22 -0700

    Improved site page override checks, cleaned up code.

--- activerules/server/activerules-view-resolver/index.js
+++ activerules/server/activerules-view-resolver/index.js
@@ -3,56 +3,77 @@
 var _ = require('lodash');
 
 var hbs = require('handlebars');
+
 // Promisified core Node functions
 var fs = require('mz/fs');
 
-var AR = function() {};
+var AR = function(){};
 
+/**
+ * Initializer function to set required paths in object
+ * 
+ * @param {type} defaultViewRoot
+ * @param {type} arRoot
+ * @returns {undefined}
+ */
+AR.prototype.init = function(defaultViewRoot, arRoot){
+    AR.prototype.defaultViewRoot = defaultViewRoot;
+    AR.prototype.arRoot = arRoot;
+}
+
+/**
+ * Provides site specific page template and data
+ * 
+ * @param {type} page
+ * @param {type} request
+ * @param {type} viewRoot
+ * @returns {Promise}
+ */
 AR.prototype.resolvePage = function(page, request, viewRoot) {
-  return new Promise(
+
+    var viewPath; // The path of the view regardless of root.
+    var siteView; // View served for this site
+    var template; // Compile Handlebars template
+    var html; // Compiled template with data injected
+    
+    return new Promise(
         function(resolve, reject) {
-          try {
-            var templateFile = false;
-
-              // use site specifc view if provided
-            if (typeof request.ar.site.viewRoot != 'undefined') {
-              var siteViewRoot = request.ar.site.viewRoot;
-
-              try {
-                templateFile = require.resolve(siteViewRoot + 'page/' + page + '.hbs');
-              } catch (err) {
-                        // Np, lets use the default view
-              }
+            
+            viewPath = '/page/' + page + '.hbs';
+            
+            siteView = AR.prototype.arRoot + '/site/' + request.ar.site.site + '/views/' + viewPath;
+            
+            if(!fs.existsSync(siteView)) {
+                siteView = AR.prototype.defaultViewRoot + viewPath;
             }
-
-            if (!templateFile) {
-              templateFile = require.resolve(viewRoot + 'page/' + page + '.hbs');
+            
+            // Try loading the view w/ data
+            try {
+                fs.readFile(siteView, 'utf8')
+                 .then(function(fileContent) {
+                     template = hbs.compile(fileContent);
+                     getPageData(page, request)
+                     .then(function(pageData) {
+                         html = template(pageData);
+                         resolve(html);
+                     });
+                 });  
+            } catch (err) {
+              reject(err);
             }
-
-            fs.readFile(templateFile, 'utf8')
-                  .then(function(fileContent) {
-                    var template = hbs.compile(fileContent);
-                    getPageData(page, request)
-                      .then(function(pageData) {
-                        var html = template(pageData);
-                        resolve(html);
-                      })
-                      .catch(function(err) {
-                        throw err;
-                      });
-                  })
-                  .catch(function(err) {
-                    throw err;
-                  });
-          } catch (err) {
-            reject(err);
-          }
         }
     );
 };
 
+/**
+ * This merges the site page data for a locale over the default page data for a locale.
+ * 
+ * @param {type} page
+ * @param {type} request
+ * @returns {Promise}
+ */
 function getPageData(page, request) {
-  return new Promise(
+    return new Promise(
         function(resolve, reject) {
           try {
             resolve({test: 'data'});
